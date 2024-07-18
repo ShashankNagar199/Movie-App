81040de728080dd4001636fe9559f043c2926ddf
ShashankNagar199 <71090810+ShashankNagar199@users.noreply.github.com>
2024-07-18 15:38:28 +0530

    Update ViewMovieAllDetails.js

--- src/components/moviesOperations/ViewMovieAllDetails.js
+++ src/components/moviesOperations/ViewMovieAllDetails.js
@@ -24,20 +24,20 @@ const ViewMovieAllDetails = () => {
         <span className="close-button" onClick={closeModalHandler}>&times;</span>
         <h2>{selectedMovie.Title}</h2>
         {selectedMovie.Poster && <img style={{height:"300px"}} src={selectedMovie.Poster} alt={selectedMovie.Title} />}
-        <p><strong>Year:</strong> {selectedMovie.Year}</p>
-        <p><strong>Rated:</strong> {selectedMovie.Rated}</p>
-        <p><strong>Released:</strong> {selectedMovie.Released}</p>
-        <p><strong>Runtime:</strong> {selectedMovie.Runtime}</p>
-        <p><strong>Genre:</strong> {selectedMovie.Genre}</p>
-        <p><strong>Director:</strong> {selectedMovie.Director}</p>
-        <p><strong>Writer:</strong> {selectedMovie.Writer}</p>
-        <p><strong>Actors:</strong> {selectedMovie.Actors}</p>
-        <p><strong>Plot:</strong> {selectedMovie.Plot}</p>
-        <p><strong>Language:</strong> {selectedMovie.Language}</p>
-        <p><strong>Country:</strong> {selectedMovie.Country}</p>
-        <p><strong>Awards:</strong> {selectedMovie.Awards}</p>
-        <p><strong>IMDB Rating:</strong> {selectedMovie.imdbRating}</p>
-        <p><strong>IMDB Votes:</strong> {selectedMovie.imdbVotes}</p>
+        <p><strong>Year:</strong> {selectedMovie.Year ?selectedMovie.Year:'...loading'}</p>
+        <p><strong>Rated:</strong> {selectedMovie.Rated ?selectedMovie.Rated:'...loading'}</p>
+        <p><strong>Released:</strong> {selectedMovie.Released ?selectedMovie.Released:'...loading'}</p>
+        <p><strong>Runtime:</strong> {selectedMovie.Runtime ?selectedMovie.Runtime:'...loading'}</p>
+        <p><strong>Genre:</strong> {selectedMovie.Genre ?selectedMovie.Genre:'...loading'}</p>
+        <p><strong>Director:</strong> {selectedMovie.Director ?selectedMovie.Director:'...loading'}</p>
+        <p><strong>Writer:</strong> {selectedMovie.Writer ?selectedMovie.Writer:'...loading'}</p>
+        <p><strong>Actors:</strong> {selectedMovie.Actors ?selectedMovie.Actors:'...loading'}</p>
+        <p><strong>Plot:</strong> {selectedMovie.Plot ?selectedMovie.Plot:'...loading'}</p>
+        <p><strong>Language:</strong> {selectedMovie.Language ?selectedMovie.Language:'...loading'}</p>
+        <p><strong>Country:</strong> {selectedMovie.Country ?selectedMovie.Country:'...loading'}</p>
+        <p><strong>Awards:</strong> {selectedMovie.Awards ?selectedMovie.Awards:'...loading'}</p>
+        <p><strong>IMDB Rating:</strong> {selectedMovie.imdbRating ?selectedMovie.imdbRating:'...loading'}</p>
+        <p><strong>IMDB Votes:</strong> {selectedMovie.imdbVotes ?selectedMovie.imdbVotes:'...loading'}</p>
         <button className="btn btn-danger" onClick={closeModalHandler}>Close</button>
       </div>
     </div>
